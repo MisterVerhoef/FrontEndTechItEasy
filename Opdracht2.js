fab6c2e181d7d2f407157050ad5b14b43bc17fa7
MisterVerhoef <frankverhoef@outlook.com>
2022-04-10 14:59:07 +0200

    bij opdracht 4

---
+++ Opdracht2.js
@@ -0,0 +1,48 @@
+// * **Opdracht 2a:** Hoeveel tv's zijn er al verkocht? Schrijf een script dat dit berekent. Log de uitkomst in de console.
+const amountOfTvsSold = inventory.map((tv) => {
+
+// for (let i = 0; i > tv.sold.length; i++){
+//
+// }
+
+   return tv.sold;
+
+})
+
+let totalTvSold = 0;
+for(let i = 0; i < amountOfTvsSold.length; i++) {
+   totalTvSold += amountOfTvsSold[i];
+}
+
+console.log(totalTvSold);
+
+// sum = 10
+
+// * **Opdracht 2b:** Zorg ervoor dat dit aantal _in het groen_ wordt weergegeven op de pagina.
+
+const tvSold = document.getElementById("tvSold");
+tvSold.textContent = totalTvSold;
+
+// * **Opdracht 2c:** Hoeveel tv's heeft Tech It Easy ingekocht? Schrijf een script dat dit berekent. Log de uitkomst in de
+const amountOfTvsOrdered = inventory.map((tv) => {
+
+   return tv.originalStock;
+
+})
+
+let totalTvOrdered = 0;
+for(let i = 0; i < amountOfTvsOrdered.length; i++) {
+   totalTvOrdered += amountOfTvsOrdered[i];
+}
+
+console.log(totalTvOrdered);
+
+// * **Opdracht 2d:** Zorg ervoor dat dit aantal _in het blauw_ wordt weergegeven op de pagina.
+const tvBought = document.getElementById("tvBought");
+tvBought.textContent = totalTvOrdered;
+
+// * **Opdracht 2e:** Geef _in het rood_ weer hoeveel tv's er nog verkocht moeten worden.
+
+const tvLeft = document.getElementById("tvLeft");
+tvLeft.textContent = totalTvOrdered - totalTvSold;
+
---
+++ opdracht4.js
@@ -0,0 +1,92 @@
+// const tvFormats = inventory.map((tv) => {
+//     return tv.brand + " " + tv.type + " - " + "€" + tv.price + ",- " + tv.availableSizes;
+// })
+//
+// const tvInfo = document.getElementById("tv-stuff");
+// tvInfo.textContent = tvName[2];
+
+// function showListTvBrands(tvlist) {
+//     let list = document.getElementById("our-Tv-Brands-Again");
+//     tvlist.forEach((item) => {
+//         let li = document.createElement("li");
+//         li.innerText = item;
+//         list.appendChild(li);
+//     });
+// }
+//
+// showListTvBrands(tvBrands);
+
+// Maak deze gehele opdracht eerst alsof je het voor één tv doet. We gaan één tv weergeven in het volgende format:
+//
+//     ```
+//   Philips 43PUS6504/12 - 4K TV
+//   €379,-
+//   43 inch (109 cm) | 50 inch (127 cm) | 58 inch (147 cm)
+//
+//   NIKKEI NH3216SMART - HD smart TV
+//   €159,-
+//   32 inch (81 cm)
+//   ```
+//
+//     * **Opdracht 4a:** Zorg ervoor dat er een string wordt gegenereerd voor de naam van een tv. Maak een functie die één
+// enkel tv-object (zoals `inventory[0]` of `inventory[6]`) verwacht en de naam op de volgende manier
+// samenvoegt: `[merk] [type] - [naam]` zoals `Philips 43PUS6504/12 - 4K TV` of `NIKKEI NH3216SMART - HD smart TV`. Test
+// of jouw functie ook werkt wanneer er een ander tv object wordt meegegeven.
+
+
+const tvName = inventory.map((tv) => {
+    return tv.brand + " " + tv.type + " - " + tv.name;
+})
+
+const tvInfo = document.getElementById("opdracht4a");
+tvInfo.textContent = tvName[2];
+
+//
+// * **Opdracht 4b:** Zorg ervoor dat de prijs van een tv netjes geformat wordt. Maak een functie die één tv-prijs als
+// parameter verwacht (zoals `379`) en daar de volgende string van maakt: `€379,-`. Test of jouw functie ook werkt
+// wanneer er een andere tv-prijs wordt meegegeven.
+
+
+
+
+// const tvPrice = inventory.map((tv) => {
+//     return "€" + tv.price + ",-";
+// })
+
+function tvPriceFormat(){
+    return "€" + tv.price + ",-";
+}
+
+const tvPrice = inventory.map((tv) => "€" + tv.price + ",-");
+
+
+const tvPriceInfo = document.getElementById("opdracht4b");
+tvPriceInfo.textContent = tvPrice[2];
+
+
+// * **Opdracht 4c:** Zorg ervoor dat er een string wordt gegenereerd voor alle beschikbare schermgroottes van één tv, in
+// zowel _inches_ als _cm_. Doe dit door een functie te schrijven die één screen-sizes array verwacht (
+//     zoals `inventory[0].availableSizes`) en de groottes op de volgende manier
+// samenvoegt: `[schermgrootte] inches ([schermgrootte omgerekend]cm) | [schermgrootte] inches ([schermgrootte omgerekend]cm)`
+// etc. Als een tv maar één schermgrootte heeft (`[32]`) wordt de output `32 inch (81 cm)`. Wanneer een tv vier
+// schermgroottes heeft (`[43, 50, 55, 58]`) wordt de output `43 inch (109 cm) | 50 inch (127 cm) | 58 inch (147 cm)`.
+//     Test of jouw functie werkt in alle mogelijke gevallen.
+//
+
+const sizeMakeup = inventory.map((tv) => tv.availableSizes + "inch" + "(" + (tv.availableSizes * 2.54) + " cm)" );
+
+console.log(sizeMakeup[4])
+
+// const tvSizeInfo = document.getElementById("opdracht4c");
+// tvSizeInfo.textContent = tvSizeMakeUp[3];
+
+// function tvSizeMakeUp(tv) { if tv.size return tv.availableSizes + "inch" + "(" + (tv.availableSizes * 2.54) + " cm)"  }
+
+// * **Opdracht 4d:** Zorg ervoor de informatie van één van de tv's zoals het voorbeeld wordt weergegeven op de pagina.
+// Gebruik hiervoor de functies die je hebt gemaakt in opdracht 5a, 5b en 5c.
+//
+// * **Opdracht 4e:** Schrijf een functie die ALLE tv's weergeeft op de pagina zoals in het voorbeeld. Dit wil je
+// natuurlijk niet acht keer opnieuw schrijven, want nu zijn het 8 tv's, maar in de toekomst misschien wel 200!
+// Gebruik in deze functie de voorgaande functies die je hebt geschreven, om onderdelen van de data te formatten. De
+// overkoepelende "tv-generator-functie" verwacht één parameter: de volledige array met tv-objecten. Vergeet 'm niet aan
+// te roepen!
